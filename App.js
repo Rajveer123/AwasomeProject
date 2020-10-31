4826afef349afce02fc0ce3751f4bead16fae867
RajveerSingh <rajveersingh.charan@gmail.com>
2020-10-31 13:40:45 +0530

    - Split the View in to Header View For Separation of Code

--- App.js
+++ App.js
@@ -9,7 +9,6 @@
 
 import React from 'react';
 import {
-  SafeAreaView,
   StyleSheet,
   ScrollView,
   View,
@@ -18,18 +17,7 @@ import {
   Platform,
   Image
 } from 'react-native';
-
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-import { bell, logo, add, message } from './src/images';
-
-
+import Header from './src/components/Header';
 const App: () => React$Node = () => {
   return (
     <>
@@ -38,41 +26,10 @@ const App: () => React$Node = () => {
           contentInsetAdjustmentBehavior="automatic"
           style={styles.scrollView}>
           <View style={styles.body}>
-            <View style={styles.headerContainer}>
-            <View style={styles.bellIconContainer}>
-              <Image source={bell} style={styles.bellIcon}></Image>
-            </View>
-               <View style={styles.headerView}>
-              <Image source={logo} style={styles.logo}></Image>
-              <Text style={styles.headerText}>Home</Text>
-              </View>
-            <View style={styles.addIconContainer}>
-              <Image source={message} style={styles.rightIcons}></Image>
-              <Image source={add} style={styles.rightIcons}></Image>
-            </View>
-            </View>
+          <Header />
             <View style={styles.profileViewContainer}>
 
             </View>
-            {/* <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks /> */}
           </View>
         </ScrollView>
     </>
@@ -87,42 +44,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#051F4C',
     paddingTop: (Platform.OS === 'ios') ? 20 : 0,
     flex :1,
-  },
-  headerContainer: {
-    flex: 1,
-    marginTop: 0,
-    paddingHorizontal: 0,
-    height : 60,
-    justifyContent:"flex-start",
-    flexDirection:'row',
-    //backgroundColor: "red"
-  },
-  bellIconContainer:{
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  bellIcon:{
-    marginLeft: 15,
-  },
-  addIconContainer:{
-    flexDirection:'row',
-    justifyContent: "flex-end",
-    alignItems:"center"
-  },
-  rightIcons: {
-  marginRight: 15,
-},
-  headerView:{
-   flex:1,
-   flexDirection : "row",
-   justifyContent:"center",
-   alignItems:"center",
-  },
-  headerText: {
-    fontSize: 22,
-    fontWeight: '600',
-    color: Colors.white,
-  },
+  }/*,
   engine: {
     position: 'absolute',
     right: 0,
@@ -131,19 +53,18 @@ const styles = StyleSheet.create({
     marginTop: 8,
     fontSize: 18,
     fontWeight: '400',
-    color: Colors.dark,
+    //color: Colors.dark,
   },
   highlight: {
     fontWeight: '700',
   },
   footer: {
-    color: Colors.dark,
+    //color: Colors.dark,
     fontSize: 12,
     fontWeight: '600',
     padding: 4,
     paddingRight: 12,
     textAlign: 'right',
-  },
+  },*/
 });
-
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import {
+    StyleSheet,
+    View,
+    Text,
+    Image
+} from 'react-native';
+import { bell, logo, add, message } from '../images';
+const Header: () => React$Node = () => {
+    return (
+
+        <View style={styles.headerContainer}>
+            <View style={styles.bellIconContainer}>
+                <Image source={bell} style={styles.bellIcon}></Image>
+            </View>
+            <View style={styles.headerView}>
+                <Image source={logo} style={styles.logo}></Image>
+                <Text style={styles.headerText}>Home</Text>
+            </View>
+            <View style={styles.addIconContainer}>
+                <Image source={message} style={styles.rightIcons}></Image>
+                <Image source={add} style={styles.rightIcons}></Image>
+            </View>
+        </View>
+
+    );
+};
+
+const styles = StyleSheet.create({
+    headerContainer: {
+        flex: 1,
+        marginTop: 0,
+        paddingHorizontal: 0,
+        height: 60,
+        justifyContent: "flex-start",
+        flexDirection: 'row',
+    },
+    bellIconContainer: {
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    bellIcon: {
+        marginLeft: 15,
+    },
+    addIconContainer: {
+        flexDirection: 'row',
+        justifyContent: "flex-end",
+        alignItems: "center"
+    },
+    rightIcons: {
+        marginRight: 15,
+    },
+    headerView: {
+        flex: 1,
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    headerText: {
+        fontSize: 22,
+        fontWeight: '600',
+        color: 'white',
+    }
+});
+export default Header;
